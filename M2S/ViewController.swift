d5292eaad25aab71c401c90f7656333134e609c3
iorin <ioioviorinx@gmail.com>
2018-04-13 14:51:25 +0900

    見やすくしました

--- M2S/ViewController.swift
+++ M2S/ViewController.swift
@@ -12,8 +12,7 @@ import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate{
     
-    var myLocationManager: CLLocationManager!
-    
+    var myLocationManager: CLLocationManager = CLLocationManager()
     
     @IBOutlet weak var testMapView: MKMapView!
 
@@ -21,17 +20,37 @@ class ViewController: UIViewController, CLLocationManagerDelegate{
         super.viewDidLoad()
         
         //LocationManagerの生成
-        myLocationManager = CLLocationManager()
+        myLocationManager.delegate = self
+        
+        //位置情報の取得を開始する。
+        myLocationManager.startUpdatingLocation()
         
         //位置情報使用許可のリクエストを表示するメソッドの呼び出し
         myLocationManager.requestWhenInUseAuthorization()
         
         }
     
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+     
+        for location in locations{
+        //中心座標
+        let center = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        
+        //表示範囲
+        let span = MKCoordinateSpanMake(0.01, 0.01)
+        
+        //中心座標と表示範囲をマップに登録する。
+        let region = MKCoordinateRegionMake(center, span)
+        testMapView.setRegion(region, animated:true)
+        
+        }
+    }
+    
+    
     //位置情報取得に失敗したときに呼び出されるメソッド
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("error")
-
+        
       }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
