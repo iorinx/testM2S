cc120515efae25ed5368418d8b65aa09fec5be2b
iorin <ioioviorinx@gmail.com>
2018-04-19 17:49:41 +0900

    なう

--- M2S/DetailViewController.swift
+++ M2S/DetailViewController.swift
@@ -8,20 +8,87 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
 
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var restrunList: [(maker:String, name:String , link:NSURL)] = []
+    
+    var restrunURL: NSURL?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+       /* let RESURL: NSURLComponents? = NSURLComponents(string: "https://api.gnavi.co.jp/RestSearchAPI/20150630/?keyid=27917f270acba305b505d2dac2c6528f&format=json&latitude=35.181446&longitude=136.906398&range=3")
+        //リクエストに必要な情報を生成
 
-        // Do any additional setup after loading the view.
+        let req = NSURLRequest(url: restrunURL! as URL)
+        
+        let url = URL(string: "https://api.gnavi.co.jp/RestSearchAPI/20150630/?keyid=27917f270acba305b505d2dac2c6528f&format=json&latitude=35.181446&longitude=136.906398&range=3")!
+        //let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = URLSession.shared.dataTask(with: url, MTLNewLibraryCompletionHandler: {(data, response, error) in
+            if error != nil {
+                print(error!.localizedDescription)
+            } else {
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
+                    {
+                          print(json)
+                    }
+                }catch {
+                }
+            }
+        })
+        task.resume()
+        // Do any additional setup after loading the view.*/
+        
+        let url: URL = URL(string: "https://api.gnavi.co.jp/RestSearchAPI/20150630/?keyid=27917f270acba305b505d2dac2c6528f&format=json&latitude=35.181446&longitude=136.906398&range=1")!
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url), completionHandler: { (data, response, error) in
+            
+            if error != nil {
+                
+                //print(error!.localizedDescription)
+            } else {
+                
+               let json = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+            
+               print(json)
+                
+               //let json = try decoder.decode(response.self, from data!)
+            }
+            
+        })
+        task.resume()
+        
     }
 
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+ /*  //JSONのrest内のデータ構造
+    struct Rest: Codable{
+        //お店の名前
+        let name: String?
+        //お店の緯度・経度
+        let latitude: Double?
+        let longitude: Double?
+        //お店のURL
+        let url: NSURL?
+    }
+    //JSONのデータ構造
+    struct Response: Codable{
+        let total_hit_count: Int?
+        let hit_par_page: Int?
+        let rest: [Rest]?
+    }*/
+    
+    
     /*
     // MARK: - Navigation
 
--- M2S/ViewController.swift
+++ M2S/ViewController.swift
@@ -12,47 +12,86 @@ import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate{
     
+   // var latitude = 35.181446
+   // var longitude = 136.906398
+    
+     //https://apvarnavi.co.jp/RestSearchAPI/20150630/?keyid=27917f270acba305b505d2dac2c6528f&format=json&latitude=35.181446&longitude=136.906398&range=1
+    
     var myLocationManager: CLLocationManager = CLLocationManager()
     
     @IBOutlet weak var testMapView: MKMapView!
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //LocationManagerの生成
-        myLocationManager.delegate = self
-        
-        //位置情報の取得を開始する。
-        myLocationManager.startUpdatingLocation()
-        
-        //位置情報使用許可のリクエストを表示するメソッドの呼び出し
-        myLocationManager.requestWhenInUseAuthorization()
-        
-        }
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
      
-        for location in locations{
-        //中心座標
-        let center = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+     //LocationManagerの生成
+     myLocationManager.delegate = self
+     
+     //位置情報の取得を開始する。
+     myLocationManager.startUpdatingLocation()
+     
+     //位置情報使用許可のリクエストを表示するメソッドの呼び出し
+     myLocationManager.requestWhenInUseAuthorization()
+     
+     }
+     
+func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+          
+          for location in locations{
+               //中心座標
+               let center = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+               
+               //表示範囲
+               let span = MKCoordinateSpanMake(0.01, 0.01)
+               
+               //中心座標と表示範囲をマップに登録する。
+               let region = MKCoordinateRegionMake(center, span)
+               testMapView.setRegion(region, animated:true)
+               
+          }
+     
+     
+         /* myLocationManager.desiredAccuracy = kCLLocationAccuracyBest
+          myLocationManager.distanceFilter = kCLDistanceFilterNone
+     
+          //LocationManagerの生成
+          myLocationManager.delegate = self
         
-        //表示範囲
-        let span = MKCoordinateSpanMake(0.01, 0.01)
+          //位置情報の取得を開始する。
+          myLocationManager.startUpdatingLocation()
         
-        //中心座標と表示範囲をマップに登録する。
-        let region = MKCoordinateRegionMake(center, span)
-        testMapView.setRegion(region, animated:true)
+          //位置情報使用許可のリクエストを表示するメソッドの呼び出し
+          myLocationManager.requestWhenInUseAuthorization()
         
-        }
-    }
+          // MapViewを生成.
+         //let myMapView: MKMapView = MKMapView()
+         // myMapView.frame = self.view.frame
+          
+          //中心座標と表示範囲をマップに登録する。
+         // let region = MKCoordinateRegionMake(center, span)
+          //testMapView.setRegion(region, animated:true)
+     
+          // MapViewにregionを追加.
+          //testMapView.region = region
+     
+          // viewにMapViewを追加.
+         // self.view.addSubview(testMapView)
+     
+          //let myPin: MKPointAnnotation = MKPointAnnotation()
+     
+          // 座標を設定.
+          //myPin.coordinate = center
+     
+          // MapViewにピンを追加.
+          //testMapView.addAnnotation(myPin)*/
+        
+     }
+     
+     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+          print("error")
+     }
+
     
-    
-    //位置情報取得に失敗したときに呼び出されるメソッド
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print("error")
-        
-      }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -60,18 +99,48 @@ class ViewController: UIViewController, CLLocationManagerDelegate{
     
     
     @IBAction func range300Button(_ sender: Any) {
-        print(300)
-    }
+          func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+          
+          //詳細画面を取得する
+          let detailViewController = segue.destination as! DetailViewController
+          
+          //詳細画面に表示するURL名を生成
+          let detailURLName = NSURL(string: "https://api.gnavi.co.jp/RestSearchAPI/20150630/?keyid=27917f270acba305b505d2dac2c6528f&format=json&latitude=35.181446&longitude=136.906398&range=1")
+          //詳細画面にURLを渡す
+          detailViewController.restrunURL = detailURLName
+     }
+     
+     }
     
     @IBAction func range500Button(_ sender: Any) {
-        print(500)
+
+          func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+          
+               //詳細画面を取得する
+               let detailViewController = segue.destination as! DetailViewController
+          
+               //詳細画面に表示するURL名を生成
+               let detailURLName = NSURL(string: "https://api.gnavi.co.jp/RestSearchAPI/20150630/?keyid=27917f270acba305b505d2dac2c6528f&format=json&latitude=35.181446&longitude=136.906398&range=2")
+               //詳細画面にURLを渡す
+               detailViewController.restrunURL = detailURLName
+          }
     }
     
     @IBAction func range1000Button(_ sender: Any) {
-        print(1000)
+     
+          func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+          
+               //詳細画面を取得する
+               let detailViewController = segue.destination as! DetailViewController
+          
+               //詳細画面に表示するURL名を生成
+               let detailURLName = NSURL(string: "https://api.gnavi.co.jp/RestSearchAPI/20150630/?keyid=27917f270acba305b505d2dac2c6528f&format=json&latitude=35.181446&longitude=136.906398&range=3")
+               //詳細画面にURLを渡す
+               detailViewController.restrunURL = detailURLName
+          }
+     }
     }
     
     
 
-}
 
